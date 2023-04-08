966d055a9777d2d48c0383a373121fc928864278
Matthew Cook <cookm353@gmail.com>
2023-04-08 14:27:57 -0400

    Added error handling and event handler

--- src/CardDeck.js
+++ src/CardDeck.js
@@ -3,7 +3,6 @@ import axios from "axios";
 import { Button } from 'reactstrap'
 import Card from "./Card";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import Card from "./Card";
 
 const CardDeck = () => {
     const [deckId, setDeckId] = useState(null)
@@ -14,34 +13,43 @@ const CardDeck = () => {
     useEffect(() => {
         const newDeckURL = "https://deckofcardsapi.com/api/deck/new/"
         const getDeck = async () => {
-            const resp = await axios.get(newDeckURL)
-            setDeckId(resp.data.deck_id)
+            try {
+                const resp = await axios.get(newDeckURL)
+                setDeckId(resp.data.deck_id)
+            } catch (err) {
+                console.log(err)
+            }
+            
         }
         getDeck()
-    }, [deckId])
+    }, [])
 
-    const handleDraw = () => {
-        useEffect(() => {
-            const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
-            console.log(deckId)
-            const drawCard = async
-            // console.log()
-        })
-    }
+    /**
+     * 
+     */
 
-    // Draw a card
+    // Handle drawing new card
     useEffect(() => {
+        const drawBttn = document.querySelector("#drawCard")
+        const url = `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
         
+        const handleDraw = async () => {
+            try {
+                console.log(`Deck id: ${deckId}`)
+                const resp = await axios.get(url)
+                console.log(resp.data)
+            } catch (err) {
+                console.log(err)
+            }
+        }
+        drawBttn.addEventListener("click", handleDraw)
     })
 
     return (
         <div>
-            <Button onClick={handleDraw} className="mb-3 mt-3">Hit me!</Button>
+            <Button id="drawCard" className="mb-3 mt-3">Hit me!</Button>
             <Card url={"https://deckofcardsapi.com/static/img/QS.png"}/>
         </div>
-        // <div>
-        //     <Button>Hit me!</Button>
-        // </div>
     )
 }
 
